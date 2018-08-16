725b7e174f82814dadfd8b1c64b31f74d4b51186
Julio Carneiro <jcarneiro@iMac-de-Administrador.local>
2018-08-16 10:06:36 -0300

    ïnitial

---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const shell = require("shelljs");
+
+const init = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("FILECREATOR", {
+        font: "epic",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  );
+};
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "Digite o nome do arquivo que deseja criar:"
+    },
+    {
+      type: "list",
+      name: "EXTENSION",
+      message: "Qual a extensao do seu arquivo?",
+      choices: [".rb", ".js", ".php", ".css", ".html"],
+      filter: function(val) {
+        return val.split(".")[1];
+      }
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const createFile = (filename, extension) => {
+  const filePath = `${process.cwd()}/${filename}.${extension}`
+  shell.touch(filePath);
+  return filePath;
+};
+
+const success = filepath => {
+  console.log(
+    chalk.white.bgGreen.bold(`Feito! Arquivo criado em: ${filepath}`)
+  );
+};
+
+const run = async () => {
+  init();
+
+  const answers = await askQuestions();
+  const { FILENAME, EXTENSION } = answers;
+
+  const filePath = createFile(FILENAME, EXTENSION);
+
+  success(filePath);
+};
+
+run();
